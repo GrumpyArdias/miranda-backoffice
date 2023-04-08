1993706cee8b8dacecc045731adb9c9470a31ab0
GrumpyArdias <this.mario@gmail.com>
2023-04-08 14:40:48 +0200

    Book half done, placeholder

--- src/components/pages/Book.jsx
+++ src/components/pages/Book.jsx
@@ -9,6 +9,12 @@ import {
   Checks,
   BookLeftSecondBlock,
   BookLeftThirdBlock,
+  CheckIn,
+  CheckOut,
+  Info,
+  RoomInfo,
+  RoomPrice,
+  TextInfo,
 } from "../styles/Book.styles";
 import { useParams } from "react-router-dom";
 import Bookings from "../../data/bookings.json";
@@ -17,13 +23,9 @@ import Rooms from "../../data/rooms.json";
 function Book() {
   const { id } = useParams();
 
-  const book = Bookings.find((obj) => obj.id === id);
+  const book = Bookings.find((obj) => obj.room_id === id);
   const room = Rooms.find((obj) => obj.id === id);
 
-  console.log(`esto es el book ${book}`);
-  console.log(`esto es el room ${room}`);
-
-  console.log(`Esto es el ID de Book ${id}`);
   return (
     <BookWrapper className="BookWrapper">
       <BookLeft className="BookLeft">
@@ -31,13 +33,41 @@ function Book() {
           <UserInfo className="userInfo">
             <UserPhoto className="UserPhoto"> </UserPhoto>
             <UserData className="UserData">
-              <h3>Placeholder</h3>
+              <h3>{book.full_name}</h3>
               <p>ID {id}</p>
             </UserData>
           </UserInfo>
-          <Checks className="Checks"></Checks>
+          <Checks className="Checks">
+            <CheckIn className="CheckIn">
+              <p>Check In</p>
+              <h3>{book.check_in}</h3>
+            </CheckIn>
+            <CheckOut className="CheckOut">
+              <p>Check Out</p>
+              <h3>{book.check_out}</h3>
+            </CheckOut>
+          </Checks>
         </BookLeftFirstBlock>
-        <BookLeftSecondBlock className="BookLeftSecondBlock"></BookLeftSecondBlock>
+        <BookLeftSecondBlock className="BookLeftSecondBlock">
+          <Info className="info">
+            <RoomInfo className="RoomInfo">
+              <p>Room Info</p>
+              <h3>{room.bed_type.toUpperCase()}</h3>
+            </RoomInfo>
+            <RoomPrice className="Price">
+              <p>Room Price</p>
+              <h3>$150 / Night</h3>
+            </RoomPrice>
+          </Info>
+          <TextInfo className="TextInfo">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi
+              deleniti unde ipsam, corrupti, labore iusto consequuntur,
+              provident odio et ducimus eligendi incidunt eius quam sit quia
+              tempora eos! Odio?
+            </p>
+          </TextInfo>
+        </BookLeftSecondBlock>
         <BookLeftThirdBlock className="BookLeftThirdBlock"></BookLeftThirdBlock>
       </BookLeft>
       <BookRight className="BookRight"></BookRight>
--- src/components/styles/Book.styles.js
+++ src/components/styles/Book.styles.js
@@ -1,26 +1,28 @@
 import styled from "@emotion/styled";
 
 export const BookWrapper = styled.div`
-  max-width: 90%;
   margin: 20px auto;
   display: flex;
   box-shadow: rgba(0, 0, 0, 0.08) 0px 20px 30px;
   border: 1px solid black;
   margin-top: 40px;
+  padding-top: 20px;
+  padding-bottom: 20px;
 `;
 export const BookLeft = styled.div`
   max-width: 50%;
-  order: 1;
+  margin-left: 30px;
 `;
 
 export const BookRight = styled.div``;
 export const BookLeftFirstBlock = styled.div`
   display: flex;
   flex-direction: column;
+  border-bottom: 1px solid #799283;
 `;
 export const UserInfo = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 `;
 export const UserPhoto = styled.div`
@@ -30,10 +32,77 @@ export const UserPhoto = styled.div`
   border-radius: 10px;
 `;
 export const UserData = styled.div`
-  max-width: 50%;
   margin-left: 10px;
+  display: flex;
+  white-space: nowrap;
+  flex-direction: column;
+  justify-content: center;
+  h3 {
+    text-align: center;
+    font-size: 30px;
+    margin-bottom: -10px;
+  }
+  p {
+    text-align: center;
+    font-size: 10px;
+    color: #799283;
+  }
 `;
-export const Checks = styled.div``;
+export const Checks = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+export const CheckIn = styled.div`
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 14px;
+    color: #799283;
+  }
+  h3 {
+    margin-top: -10px;
+  }
+`;
+export const CheckOut = styled.div`
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 14px;
+    color: #799283;
+  }
+  h3 {
+    margin-top: -10px;
+  }
+`;
 export const BookLeftSecondBlock = styled.div``;
+export const Info = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+export const RoomInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 14px;
+    color: #799283;
+  }
+  h3 {
+    margin-top: -10px;
+  }
+`;
+
+export const RoomPrice = styled.div`
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 14px;
+    color: #799283;
+  }
+  h3 {
+    margin-top: -10px;
+  }
+`;
+
+export const TextInfo = styled.div``;
 
 export const BookLeftThirdBlock = styled.div``;
